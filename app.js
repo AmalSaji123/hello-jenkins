667ae927106874e5ec4a31f7263f0de6a65f9d39
Amal Saji <amal.saji@ibm.com>
2026-05-12 15:18:28 +0530

    latest

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins CI/CD! 1234');
+  res.send('Hello from Jenkins CI/CD! 123');
 });
 
 app.get('/hello', (req, res) => {
